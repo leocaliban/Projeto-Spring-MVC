63da0910842df0d733d137594a6ba6cb7b565e81
Leocaliban <leocaliban@gmail.com>
2017-09-01 03:39:44 -0300

    Aula 2.6 - Resolvendo status com thymeleaf (Combo Dinâmico)

--- cobranca/src/main/java/com/caliban/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/caliban/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.caliban.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.caliban.cobranca.model.StatusTitulo;
 import com.caliban.cobranca.model.Titulo;
 import com.caliban.cobranca.repository.Titulos;
 
@@ -17,8 +22,9 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -27,7 +33,11 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem","Título Cadastrado Com Sucesso!");
 		return mv;
-
 	}
-
+	
+	/* os status de titulos será chamado pelo TL por aqui, que tem um array dos status*/
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo>todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
 }
