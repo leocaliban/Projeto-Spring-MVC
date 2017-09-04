96a5c49e77e71e3c62f9f37f1218cbccdb6f8bb8
Leocaliban <leocaliban@gmail.com>
2017-09-04 03:10:17 -0300

    Aula 2.14 - Mantendo Valores Formulário (th:field e redirect)

--- cobranca/src/main/java/com/caliban/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/caliban/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.caliban.cobranca.model.StatusTitulo;
 import com.caliban.cobranca.model.Titulo;
@@ -31,14 +32,13 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if(errors.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}
 		titulos.save(titulo);	
-		mv.addObject("mensagem","Título Cadastrado Com Sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título Salvo Com Sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
