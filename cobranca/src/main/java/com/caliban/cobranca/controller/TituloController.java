351336b6704816397dbeecb6549e791f0b7a75b5
Leocaliban <leocaliban@gmail.com>
2017-09-04 04:22:45 -0300

    Aula 2.15 - Criando a Edição e Evitando Salvamento Duplicado

--- cobranca/src/main/java/com/caliban/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/caliban/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,13 +21,14 @@ import com.caliban.cobranca.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 	
 	@Autowired
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
@@ -34,7 +36,7 @@ public class TituloController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if(errors.hasErrors()) {
-			return "CadastroTitulo";
+			return "CADASTRO_VIEW";
 		}
 		titulos.save(titulo);	
 		attributes.addFlashAttribute("mensagem", "Título Salvo Com Sucesso!");
@@ -46,6 +48,19 @@ public class TituloController {
 		List<Titulo>todosTitulos = titulos.findAll();
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	//faz op mapeamento do codigo
+	@RequestMapping("{codigo}")
+	public ModelAndView editar(@PathVariable("codigo")Titulo titulo) {
+		/*recupera o titulo do codigo mapeado que foi convertido em long
+		Titulo titulo = titulos.findOne(codigo);
+		*/
+		//cria a página
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		//adiciona ná página o conteudo recuperado
+		mv.addObject(titulo);
 		return mv;
 	}
 	
